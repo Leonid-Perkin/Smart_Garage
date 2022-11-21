e19a54d0bbdf2c56f8f7139aecfe0a6ce5ddf73f
Leonid27-07 <60135318+Leonid27-07@users.noreply.github.com>
2022-11-21 21:55:13 +0300

    Update sketch_oct10a.ino
    Стабильная прошивка панели управления

--- sketch_oct10a.ino
+++ sketch_oct10a.ino
@@ -7,6 +7,10 @@ const char buttonPin2 = 4;
 const char buttonPin3 = 5;
 const char buttonPin4 = 6;
 const char buttonPin5 = 7;
+const char led1 = 9;
+const char led2 = 10;
+const char led3 = 11;
+const char led4 = 12;
 char buttonState = 0;
 char buttonState1 = 0;
 char buttonState2 = 0;
@@ -37,6 +41,10 @@ void setup()
   pinMode(buttonPin3, INPUT);
   pinMode(buttonPin4, INPUT);
   pinMode(buttonPin5, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -46,12 +54,42 @@ void loop()
   if (buf) {
     char cBuf = Serial.read();
     switch (cBuf) {
+      case 'Y': {
+          print_text("превышение", "co2");
+          digitalWrite(led2, HIGH);
+        }
+        break;
+      case 'Z': {
+          print_text("есть", "обнавления");
+          digitalWrite(led4, HIGH);
+        }
+        break;
+      case 'S': {
+          digitalWrite(led4, LOW);
+        }
+        break;
+      case 'W': {
+          print_text("норма", "co2");
+          digitalWrite(led2, LOW);
+        }
+        break;
+        case 'M': {
+          print_text("Alarm", "сигнализация");
+          digitalWrite(led3, HIGH);
+        }
+        break;
+      case 'N': {
+          digitalWrite(led3, LOW);
+        }
+        break;
       case 'A': {
           print_text("Сигнализация", "on");
+          digitalWrite(led1, HIGH);
         }
         break;
       case 'B': {
           print_text("Сигнализация", "off");
+          digitalWrite(led1, LOW);
 
         }
         break;
@@ -70,12 +108,13 @@ void loop()
       case 'F': {
           print_text("лампа", "off");
         }
+        break;
       case 'G': {
           lcd.clear();
           lcd.setCursor(0, 0);
-          lcd.print("Прошивка test");
+          lcd.print("Прошивка stable");
           lcd.setCursor(0, 1);
-          lcd.print("10.5");
+          lcd.print("11.3.2");
           delay(2000);
           lcd.clear();
           lcd.setCursor(0, 0);
@@ -106,12 +145,12 @@ void loop()
     }
   }
   buttonState = digitalRead(buttonPin);
-  if (buttonState == HIGH) {
+  if (buttonState1 == HIGH) {
     delay(200);
     Serial.print('a');
   }
   buttonState1 = digitalRead(buttonPin1);
-  if (buttonState1 == HIGH) {
+  if (buttonState == HIGH) {
     delay(200);
     Serial.print('b');
   }
