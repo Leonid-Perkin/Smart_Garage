760c6108186ce69ee614af115f0162285e884511
Leonid27-07 <leonid.perkin@gmail.com>
2023-02-11 17:14:19 +0300

    add core esp

---
+++ esp-leonid/scr/esphome/core/defines.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "esphome/core/macros.h"
+#define ESPHOME_BOARD "esp32dev"
+#define ESPHOME_VARIANT "ESP32"
+#define USE_API
+#define USE_API_NOISE
+#define USE_ARDUINO_VERSION_CODE VERSION_CODE(2, 0, 5)
+#define USE_BINARY_SENSOR
+#define USE_CAPTIVE_PORTAL
+#define USE_COVER
+#define USE_JSON
+#define USE_LIGHT
+#define USE_LOGGER
+#define USE_MDNS
+#define USE_OTA
+#define USE_OTA_PASSWORD
+#define USE_SENSOR
+#define USE_SOCKET_IMPL_BSD_SOCKETS
+#define USE_SWITCH
+#define USE_WEBSERVER
+#define USE_WEBSERVER_LOCAL
+#define USE_WEBSERVER_PORT 80
+#define USE_WEBSERVER_VERSION 2
+#define USE_WIFI
